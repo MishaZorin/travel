ba92b3da39cf6d758bfb036b6e83571b7a2dbb5c
BMW <zorin.mikhail.e@gmail.com>
2025-06-08 11:12:18 +0300

    js

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ nextButton.onclick = function (){
 prevButton.onclick = function (){
   clicks--
   if(clicks == -1){
-    clicks = 2
+    clicks = 0
   }
   slider.style.transform = 'translateX(calc(-100% / 3 * ' + clicks + '))'
 
